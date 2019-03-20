72ca8735b79e5b33d56f2a4d79a603a39b5efe03
Arun <arun12singh0212@gmail.com>
2019-03-20 10:04:53 +0530

    test beans

---
+++ CapBook/src/main/java/com/cg/capbook/CapBookApplication.java
@@ -0,0 +1,13 @@
+package com.cg.capbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapBookApplication.class, args);
+	}
+
+}
